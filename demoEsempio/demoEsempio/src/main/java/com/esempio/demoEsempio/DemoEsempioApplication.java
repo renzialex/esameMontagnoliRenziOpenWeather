5294df8329aa44b8ab79212256e46654d1af7fc5
saramontagnoli <95367415+saramontagnoli@users.noreply.github.com>
2022-01-07 16:22:25 +0100

    Inserimento progetto SpringBoot

---
+++ demoEsempio/demoEsempio/src/main/java/com/esempio/demoEsempio/DemoEsempioApplication.java
@@ -0,0 +1,13 @@
+package com.esempio.demoEsempio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEsempioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEsempioApplication.class, args);
+	}
+
+}
---
+++ demoEsempio/demoEsempio/src/test/java/com/esempio/demoEsempio/DemoEsempioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esempio.demoEsempio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEsempioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
